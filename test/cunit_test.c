488a14b94a327ef6db3623ebc146dadf326de7fd
Julian Bouzas <julian.bouzas@vcatechnology.com>
2015-07-22 16:54:48 +0100

    cunit_test: Added 'add_remove_client' and 'set_curr_stack' unit tests

--- test/cunit_test.c
+++ test/cunit_test.c
@@ -2,44 +2,64 @@
 #include <string.h>
 #include <CUnit/Basic.h>
 
+#include "../neuro/stackset.h"
 #include "../neuro/neurowm.h"
 
-int init_suite1(void) {
+int init_stackset_suite(void) {
+  setConfigB(&defWMConfig);
+  if (!initSS())
+    return -1;
   return 0;
 }
 
-int clean_suite1(void) {
+int clean_stackset_suite(void) {
+  endSS();
   return 0;
 }
 
 // Test 1
-void test1(void) {
-  CU_ASSERT(0 == 0);
+void add_remove_client(void) {
+  // Create a fake client
+  const Window w = 0;
+  const XWindowAttributes wa = (XWindowAttributes){.x=0, .y=0, .width=500, .height=500};
+  Client *cli = allocClientG(w, &wa);
+  CU_ASSERT_PTR_NOT_NULL(cli);
+
+  // Add it to the stackset
+  CliPtr c = addCliStartSS(cli);
+  CU_ASSERT_PTR_NOT_NULL(c);
+
+  // Remove it from the stackset
+  Client *cli2 = rmvCliSS(c);
+  CU_ASSERT_PTR_NOT_NULL(cli2);
+  CU_ASSERT(cli == cli2);
+
+  // Free the fake client
+  freeClientG(cli2);
 }
 
 // Test 2
-void test2(void) {
-  CU_ASSERT(1 == 1);
+void set_curr_stack(void) {
+  setCurrStackSS(1);
+  CU_ASSERT(getCurrStackSS() == 1);
 }
 
 // Main
 int main() {
-  CU_pSuite pSuite = NULL;
-
   // Initialize the CUnit test registry
   if (CUE_SUCCESS != CU_initialize_registry())
     return CU_get_error();
 
   // Add a suite to the registry
-  pSuite = CU_add_suite("Suite_1", init_suite1, clean_suite1);
-  if (NULL == pSuite) {
+  CU_pSuite stackset_suite = CU_add_suite("StackSet_Suite", init_stackset_suite, clean_stackset_suite);
+  if (NULL == stackset_suite) {
     CU_cleanup_registry();
     return CU_get_error();
   }
 
   // Add the tests to the suite
-  if ((NULL == CU_add_test(pSuite, "test of test1()", test1)) ||
-      (NULL == CU_add_test(pSuite, "test of test2()", test2))) {
+  if ((NULL == CU_add_test(stackset_suite, "add_remove_client()", add_remove_client)) ||
+      (NULL == CU_add_test(stackset_suite, "set_curr_stack()", set_curr_stack))) {
     CU_cleanup_registry();
     return CU_get_error();
   }
