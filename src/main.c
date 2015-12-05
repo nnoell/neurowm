4cb2fe10adbbb01ba07c0c7ab9836bda603e3743
Julian <nnoell3@gmail.com>
2015-12-05 11:11:15 +0000

    main: Made code consistent

--- src/main.c
+++ src/main.c
@@ -23,13 +23,13 @@
 //----------------------------------------------------------------------------------------------------------------------
 
 // FlagHandler
-typedef int (*const FlagHandler)();
+typedef int (*const FlagHandlerFn)();
 
 // FlagHandlers
 typedef struct FlagHandlers FlagHandlers;
 struct FlagHandlers {
   const char *const name;
-  const FlagHandler handler;
+  const FlagHandlerFn handler;
   const char *const desc;
 };
 
@@ -38,15 +38,15 @@ struct FlagHandlers {
 // FUNCTION DECLARATION
 //----------------------------------------------------------------------------------------------------------------------
 
-static int runCmd(const char *const *cmd);
-// static pid_t getNeurowmPid();
-static int helpHandler();
-static int versionHandler();
-static int recompileHandler();
-// static int reloadHandler();
-static int runNeurowm(int argc, const char *const *argv);
-static int runFlag(const char *flgname);
-static int loopRunNeurowm(int argc, const char *const *argv);
+static int run_cmd(const char *const *cmd);
+// static pid_t get_neurowm_pid();
+static int help_handler();
+static int version_handler();
+static int recompile_handler();
+// static int reload_handler();
+static int run_neurowm(int argc, const char *const *argv);
+static int run_flag(const char *flgname);
+static int loop_run_neurowm(int argc, const char *const *argv);
 
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -54,25 +54,25 @@ static int loopRunNeurowm(int argc, const char *const *argv);
 //----------------------------------------------------------------------------------------------------------------------
 
 // Flags                                        NAME           HANDLER           DESC
-static const FlagHandlers helpFlag[]      = { { "--help",      helpHandler,      "Print this message"           } };
-static const FlagHandlers versionFlag[]   = { { "--version",   versionHandler,   "Print the version number"     } };
-static const FlagHandlers recompileFlag[] = { { "--recompile", recompileHandler, "Recompile your configuration" } };
-// static const FlagHandlers reloadFlag[]    = { { "--reload",    reloadHandler,    "Reload the window manager"    } };
+static const FlagHandlers help_flag[]      = { { "--help",      help_handler,      "Print this message"           } };
+static const FlagHandlers version_flag[]   = { { "--version",   version_handler,   "Print the version number"     } };
+static const FlagHandlers recompile_flag[] = { { "--recompile", recompile_handler, "Recompile your configuration" } };
+// static const FlagHandlers reload_flag[]   = { { "--reload",    reload_handler,    "Reload the window manager"    } };
 
 // Flags array
-static const FlagHandlers *allFlags[] = { helpFlag, versionFlag, recompileFlag, NULL };  // Removed reloadFlag
+static const FlagHandlers *flag_set[] = { help_flag, version_flag, recompile_flag, NULL };  // Removed reload_flag
 
 
 //----------------------------------------------------------------------------------------------------------------------
 // FUNCTION DEFINITION
 //----------------------------------------------------------------------------------------------------------------------
 
-static int runCmd(const char *const *cmd) {
+static int run_cmd(const char *const *cmd) {
   assert(cmd);
   int res = 0, status;
-  pid_t pid = fork();
+  const pid_t pid = fork();
   if (pid == -1) {
-    perror("runCmd - Could not fork");
+    perror("run_cmd - Could not fork");
     exit(EXIT_FAILURE);
   }
   if (!pid) {  // Child
@@ -83,48 +83,43 @@ static int runCmd(const char *const *cmd) {
   return res == -1 ? res : status;
 }
 
-// static pid_t getNeurowmPid() {
-//   char pidstr[ NAME_MAX ];
-//   FILE *cmd = popen("/usr/bin/pidof -s " WM_MYNAME, "r");
-//   fgets(pidstr, NAME_MAX, cmd);
-//   pid_t wmpid = (pid_t)strtoul(pidstr, NULL, 10);
-//   if (wmpid <= 0) // WM not running
-//   return 0;
-//   return wmpid;
-// }
+//  static pid_t get_neurowm_pid() {
+//    char pidstr[ NAME_MAX ];
+//    FILE *cmd = popen("/usr/bin/pidof -s " WM_MYNAME, "r");
+//    fgets(pidstr, NAME_MAX, cmd);
+//    return (pid_t)strtoul(pidstr, NULL, 10);
+//  }
 
-static int helpHandler() {
+static int help_handler() {
   printf("Usage: neurowm [OPTION]\nOptions:\n");
   int i;
-  for (i=0; allFlags[ i ]; ++i)
-    printf("  %s\t\t%s\n", allFlags[ i ]->name, allFlags[ i ]->desc);
+  for (i=0; flag_set[ i ]; ++i)
+    printf("  %s\t\t%s\n", flag_set[ i ]->name, flag_set[ i ]->desc);
   return 0;
 }
 
-static int versionHandler() {
+static int version_handler() {
   printf(WM_NAME " " VERSION "\n");
   return 0;
 }
 
-static int recompileHandler() {
+static int recompile_handler() {
   char out[ NAME_MAX ] = "", src[ NAME_MAX ] = "";
   snprintf(out, SNPRINTF_SIZE, "%s/." WM_NAME "/" WM_MYNAME, getenv("HOME"));
   snprintf(src, SNPRINTF_SIZE, "%s/." WM_NAME "/" WM_NAME ".c", getenv("HOME"));
   char lib[ NAME_MAX ] = "/usr/lib/neuro/lib" WM_NAME ".a";
   const char *const cmd[] = { "/usr/bin/cc", "-O3", "-o", out, src, lib, "-lX11", "-pthread", NULL };
-  return runCmd(cmd);
+  return run_cmd(cmd);
 }
 
-// static int reloadHandler() {
-//   pid_t wmpid = getNeurowmPid();
-//   if (wmpid == 0)
-//     return 0;
-//   if (kill(wmpid, SIGUSR1) == -1)
-//     return -1;
-//   return 0;
-// }
+//  static int reload_handler() {
+//    const pid_t wmpid = get_neurowm_pid();
+//    if (wmpid <= 0)
+//      return 0;
+//    return kill(wmpid, SIGUSR1) == -1 ? -1 : 0;
+//  }
 
-static int runNeurowm(int argc, const char *const *argv) {
+static int run_neurowm(int argc, const char *const *argv) {
   assert(argv);
   const char *cmd[ argc + 1 ];
   char bin[ NAME_MAX ] = "";
@@ -134,15 +129,15 @@ static int runNeurowm(int argc, const char *const *argv) {
   int i;
   for (i = 1; i < argc; ++i)
     cmd[ i ] = argv[ i ];
-  return runCmd((const char *const *)cmd);
+  return run_cmd((const char *const *)cmd);
 }
 
-static int runFlag(const char *flgname) {
+static int run_flag(const char *flgname) {
   assert(flgname);
   int i;
-  for (i=0; allFlags[ i ]; ++i)
-    if (!strcmp(flgname, allFlags[ i ]->name)) {
-      int res = allFlags[ i ]->handler();
+  for (i=0; flag_set[ i ]; ++i)
+    if (!strcmp(flgname, flag_set[ i ]->name)) {
+      int res = flag_set[ i ]->handler();
       if (res == -1)
         perror(flgname);
       return res;
@@ -150,11 +145,11 @@ static int runFlag(const char *flgname) {
   return 1;
 }
 
-static int loopRunNeurowm(int argc, const char *const *argv) {
+static int loop_run_neurowm(int argc, const char *const *argv) {
   assert(argv);
   int res;
   do {
-    res = runNeurowm(argc, argv);
+    res = run_neurowm(argc, argv);
   } while (WEXITSTATUS(res) == EXIT_RELOAD);
   return res;
 }
@@ -168,12 +163,12 @@ int main(int argc, const char *const *argv) {
   if (argc > 1) {
     int i, res;
     for (i = 1; i < argc; ++i) {
-      res = runFlag(argv[ i ]);
+      res = run_flag(argv[ i ]);
       runwm = res != -1 && res != 0;  // res: -1 error, 0 dont run, 1 run wm
     }
   }
   int reswm = 0;
   if (runwm)
-    reswm = loopRunNeurowm(argc, argv);
+    reswm = loop_run_neurowm(argc, argv);
   return reswm == -1 ? EXIT_FAILURE : EXIT_SUCCESS;
 }
